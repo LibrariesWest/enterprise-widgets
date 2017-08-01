596facc8b52643f9da7cd2bdc49d97e23c71679d
DaveBathnes <dxrowe@somerset.gov.uk>
2017-08-01 17:20:17 +0100

    goodreads widget start

---
+++ goodreadsReviews.js
@@ -0,0 +1,12 @@
+/*
+    Type: Script Only
+    Code: GOODREADSREVIEWS
+    Name: Gooderads Reviews
+    Description: Displays Goodreads reviews in the accordion view on the item detail page
+    Selected fields: ISBN
+    Bound JavaScript Function: goodreadsReviewsDisplay
+*/
+
+function goodreadsReviewsDisplay() {
+
+}
--- moreDetails.js
+++ moreDetails.js
@@ -12,7 +12,7 @@ function moreDetailsResultLink() {
     if (resultdisplays != null && resultdisplays.length > 0) {
         jQuery.each(resultdisplays, function(i, r) {
             var resultid = r.id.replace('results_cell', '');
-            var moredetailsurl = jQuery('div.results_cell#results_cell' + resultid + ' a#detailLink' + resultid).attr('href');
+            var moredetailsurl = jQuery('div.results_cell#results_cell' + resultid + ' a#detailLink' + i).attr('href');
             var moredetailsbtn = '<div class="amazonButton"><input class="button" title="See further details" onclick="window.location = \'https://www.librarieswest.org.uk' + moredetailsurl + '\'" value="More details" type="button"></div>';
             var resultsbtnsdiv = jQuery('div#results_cell' + resultid +' div.results_right');
             if (resultsbtnsdiv != null && resultsbtnsdiv.length > 0) resultsbtnsdiv.append(moredetailsbtn);
